fe5da3e183368224acbec323c66cdaf21e3cfccd
mp4402 <mariopisquiy@ufm.edu>
2021-04-23 18:22:26 -0600

    Corrección de métodos
    Se corrigieron los métodos delete_queue, delete_list y eliminarCancion

--- app.py
+++ app.py
@@ -31,9 +31,26 @@ def añadirCancion(nombre,artista,album)-> None:
     listadoCanciones.insertar(datos[0],datos[1],datos[2])
 
 def eliminarCancion():
+    cont = -1
+    i = 0
+    lista = ["","",""]
+    cancion = listadoCanciones.head
     with open('listado.csv') as File:
+        reader = csv.reader(File,delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            cont = cont+1
+    with open('listado.csv','w',newline='') as writeFile:
         writer = csv.writer(writeFile, delimiter=",", quotechar=',',quoting=csv.QUOTE_MINIMAL,lineterminator='')
-        
+        while (i != cont):
+            lista[0] = cancion.nombre
+            lista[1] = cancion.artista
+            lista[2] = cancion.album
+            if (i != 0):
+                writer.writerow('\n')
+            writer.writerow(lista)
+            cancion = cancion.next
+            i+=1
+
 
 def cargarCanciones():
     global listadoCanciones, listaCanciones
@@ -50,32 +67,38 @@ def cargarCanciones():
 def deletequeue(cancion_eliminar, cancion):
     contador = 0
     while (True):
+        if cancion_eliminar == colaCanciones.head.nombre:
+                colaCanciones.dequeue()
+                break
         if contador == 1:
             cancion_comprobacion = colaCanciones.head.nombre
         if cancion.next is not None:
-            if cancion_eliminar == colaCanciones.head.nombre:
-                colaCanciones.dequeue
-                break
-            if cancion_eliminar == cancion.nombre.nombre:
+            if cancion_eliminar == cancion.nombre:
                 cancion_comprobacion.next = cancion.next
                 break
             else:
                 cancion = cancion.next
                 contador+=1
                 if contador >= 1:
-                    cancion_comprobacion = cancion.next
+                    cancion_comprobacion = cancion_comprobacion.next
+        else:
+            break
 
 def deletelist(cancion_eliminar, cancion):
     while (True):
         if cancion.next is not None:
             if cancion_eliminar == listadoCanciones.head.nombre:
-                listadoCanciones.head = cancion.next
+                listadoCanciones.head = listadoCanciones.head.next
                 break
             if cancion_eliminar == cancion.nombre:
+                cancion.next.previous = cancion.previous
                 cancion.previous.next = cancion.next
                 break
             else:
                 cancion = cancion.next
+        else:
+            break
+    eliminarCancion()
 
 cargarCanciones()
 #principal
@@ -138,7 +161,9 @@ def index():
         elif request.form.get('play') == 'play':
             cancionActual = listadoCanciones.head
             ultimaCancion = cancionActual
-            #colaCanciones.enqueue(Cancion("Levitating", "Dua Lipa", "Future Nostalgia"))
+            #xCancion = Cancion("Levitating", "Dua Lipa", "Future Nostalgia")
+            #colaCanciones.enqueue(xCancion)
+            #deletequeue(xCancion.nombre, colaCanciones.head)
         elif request.form.get('delete_queue') == 'delete_queue':
             cancion_eliminar = request.form.get('nombre cancion')
             cancion = colaCanciones.head
